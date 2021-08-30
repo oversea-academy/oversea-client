cfcbad13c0f81f4a12a5727ae2f33c2033dff1a6
Miftahul Arifin <miftahul97@gmail.com>
2021-08-30 17:19:29 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
@@ -13,14 +13,18 @@ export default function Home() {
 
       <main className={styles.main}>
         <Image alt="Oversea Academy" src="/Logo2.png" width={320} height={128} />
-        <h3 className={styles.title}>
-          Coming soon!
-        </h3>
+        <h3 className={styles.title}>Coming soon!</h3>
 
         <p className={styles.description}>
-          Visit our instagram <a href="https://www.instagram.com/oversea.academy/" target="_blank" rel="noreferrer">@oversea.academy</a>
+          Visit our instagram{" "}
+          <a
+            href="https://www.instagram.com/oversea.academy/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @oversea.academy
+          </a>
         </p>
-        
       </main>
     </div>
   )

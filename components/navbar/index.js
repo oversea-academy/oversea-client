14917039950b33e78270fed94a157e5345b1bed7
Miftahul Arifin <miftahul97@gmail.com>
2021-09-02 08:08:50 +0700

    update

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="navbar shadow-lg bg-neutral-content text-neutral md:px-10 lg:px-20 sticky top-0 z-30">
       <div className="flex-none px-2 mx-2">
-        <Image alt="Oversea Academy" src="/Logo2.png" width={160} height={64} />
+        <Image alt="Oversea Academy" src="/Logo.png" width={160} height={64} />
       </div>
       <div className="flex-1 px-2 mx-2">
         <div className="items-stretch hidden lg:flex">
@@ -14,10 +14,10 @@ export default function Navbar() {
         </div>
       </div>
       <div className="flex-none">
-        <a className="btn btn-ghost capitalize">Masuk</a>
+        <a className="btn btn-ghost btn-sm capitalize">Masuk</a>
       </div>
       <div className="flex-none">
-        <a className="btn btn-primary capitalize">Daftar</a>
+        <a className="btn btn-primary btn-sm px-6 capitalize">Daftar</a>
       </div>
     </div>
   );

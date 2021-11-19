954de9aa19f886b72b4e96688676897011f1ed46
Miftahul Arifin <miftahul97@gmail.com>
2021-11-19 08:23:12 +0700

    add template for terms and policy

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,11 +1,23 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
     <>
       <footer className="p-10 footer bg-primary text-neutral-content w-full flex justify-center p-3">
         <div className="w-full md:w-9/12 lg:w-6/12">
-          <span className="text-xs lg:text-sm">
-            ©Oversea Academy 2021. Hak Cipta Dilindungi | Syarat & Ketentuan | Kebijakan Privasi
-          </span>
+          <div className="flex gap-5">
+            <span className="text-xs lg:text-sm">© 2021 Oversea Academy</span>
+            <span>
+              <Link href="/terms-and-condition" passHref>
+                Ketentuan Penggunaan
+              </Link>
+            </span>
+            <span>
+              <Link href="/privacy-policy" passHref>
+                Kebijakan Privasi
+              </Link>
+            </span>
+          </div>
         </div>
       </footer>
     </>
---
+++ pages/privacy-policy.js
@@ -0,0 +1,142 @@
+import Head from 'next/head';
+
+export default function Layanan() {
+  return (
+    <div>
+      <Head>
+        <title>Oversea Academy | Kebijakan Privasi</title>
+        <meta name="description" content="Oversea Academy" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <section>
+          <div className="w-full">
+            <div className="w-full flex justify-center px-5">
+              <div className="w-full md:w-9/12 lg:w-8/12 py-16">
+                <p className="mb-2 lg:mb-3 text-primary font-bold">Oversea Academy</p>
+                <h1 className="mb-9 text-2xl md:text-3xl lg:text-4xl font-bold">Privacy Policy</h1>
+                <div>
+                  <div className="my-3">
+                    <p className="py-2">
+                      Schoters.com is owned and operated by PT. Partner Impian Milenial (“Schoters”, “Us”, “We”, or
+                      “Our”). This Privacy Policy describes how we collect, use, disclose, process and protect
+                      personally identifiable information (“Personal Data”) that you (the “User”) may provide in
+                      connection with Schoters services available through the website located at www.schoters.com
+                      (“Site”). Personal Data means data, whether true or not, about an individual. References to the
+                      Site are deemed to include derivatives thereof, including but not necessarily limited to mobile
+                      websites and applications. By visiting or using the Site, you agree and consent to the collection,
+                      use, disclosure and processing of your Personal Data in accordance with this Privacy Policy and/or
+                      the Website Terms. If you do not agree to the Privacy Policy below, please leave the Site
+                      immediately.
+                    </p>
+                    <p className="py-2">
+                      From time to time, Schoters may revise this Privacy Policy to reflect changes in the law, our
+                      Personal Data collection and use practices, the features of our Site, or advances in technology.
+                      If we make revisions that change the way we collect or use your Personal Data, those changes will
+                      be posted in this Privacy Policy and the effective date will be noted at the beginning of this
+                      Privacy Policy. Therefore, you should review this Privacy Policy periodically so that you are up
+                      to date on our most current policies and practices. Schoters will also prominently post such
+                      material changes prior to implementing the change. If you do not agree with any changes or
+                      modifications to the Privacy Policy, please do not continue using the Site. You will be deemed to
+                      have consented to any modification of the Privacy Policy when you use the Site after the effective
+                      date of the modification.
+                    </p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">1. Information We Collect</p>
+                    <p className="py-2">
+                      We collect the Personal Data that you provide to us when using the Site. This includes but is not
+                      limited to: your name, Schoters user identification and login credentials, address, phone number,
+                      email address, hotel name, hotel location, and/or length of stay. We may request that you provide
+                      information on your travel preferences, airline frequent flyer membership, as well as feedback on
+                      your travel experiences. We also collect non-personally identifiable information including but not
+                      limited to your internet protocol (IP) address, geographic location, operating system type,
+                      nationality, search preferences, as well as other generic Internet usage related data.
+                    </p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">2. How We Use Your Information</p>
+                    <p className="py-2">
+                      We may use your Personal Data and other information gathered through the Site for the following
+                      purposes:
+                    </p>
+                    <ul className="list-disc py-2 px-4">
+                      <li>registering, managing and/or administering your use and/or access of the Site</li>
+                      <li>managing, operating, administering and providing you the services offered on the Site</li>
+                      <li>
+                        contacting you on matters relating to your use and/or access of the Site and services on the
+                        Site, and any enquiries and/or requests submitted by you through the Site or otherwise
+                      </li>
+                      <li>to customize your experience when using the Site</li>
+                      <li>measuring and improving customer experience and satisfaction</li>
+                    </ul>
+                    <p className="py-2">
+                      We may also use your Personal Data and other information collected for Social Media marketing
+                      using direct and open graph techniques; for digital and conventional marketing purposes such as
+                      sending you direct mailers about new products, and special offers or other information which we
+                      think you may find interesting. Please note that you may opt out of any marketing materials we may
+                      send to you at any time. Please follow the unsubscribe instructions set out in our marketing
+                      materials if you choose to opt out of any marketing materials and we will respect your wishes.
+                    </p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">3. Protecting Your Personal Data</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">4. Credit Card Details</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">5. Payment Gateway Info</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">6. Sharing and Transferring Your Personal Data</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">7. Consent</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">8. Withdrawing Consent</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">9. Cookie Policy</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">10. Links to Other Websites</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">11. Registration and Memberships</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">12. Approval of Privacy Policy Changes</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">13. Order of Precedence</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="text-lg font-bold py-2">14. Contact Us</p>
+                    <p className="py-2"></p>
+                  </div>
+                  <div className="my-3">
+                    <p className="py-2">Effective Date December 2021</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}

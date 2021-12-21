fae5af04a901067bd0619b17e1a73bc71f3dbf7f
Miftahul Arifin <miftahul97@gmail.com>
2021-12-21 20:51:05 +0700

    feat: add profile page

--- pages/account/profile.js
+++ pages/account/profile.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import AuthenticatedRoute from '../../components/AuthenticatedRoute';
 
-export default function Profile() {
-  const [isLoading, setIsLoading] = useState(false);
+function Profile() {
+  const userProfile = useSelector((state) => state.userProfile);
 
   return (
     <div className="min-h-screen">
@@ -16,7 +18,35 @@ export default function Profile() {
         <section>
           <div className="w-full">
             <div className="w-full flex justify-center px-5">
-              <div className="w-full md:w-9/12 lg:w-8/12 py-10"></div>
+              <div className="w-full md:w-9/12 lg:w-8/12 py-10">
+                {userProfile.email ? (
+                  <div className="card shadow">
+                    <div className="card-body">
+                      <h2 className="card-title">My Profile</h2>
+                      <div className="flex flex-col gap-3">
+                        <div className="flex">
+                          <div className="w-24">Nama</div>
+                          <div>{`${userProfile.first_name} ${userProfile.last_name}`}</div>
+                        </div>
+                        <div className="flex">
+                          <div className="w-24">Email</div>
+                          <div>{userProfile.email}</div>
+                        </div>
+                        <div className="flex">
+                          <div className="w-24">Nomor HP</div>
+                          <div>{userProfile.phone_number}</div>
+                        </div>
+                        <div className="flex">
+                          <div className="w-24">Role</div>
+                          <div>{userProfile.roles.join(' ')}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  ''
+                )}
+              </div>
             </div>
           </div>
         </section>
@@ -24,3 +54,5 @@ export default function Profile() {
     </div>
   );
 }
+
+export default AuthenticatedRoute(Profile, { pathAfterFailure: '/account/signin' });

3734b6162707756b07fc214a4e05c152b3be0f47
Miftahul Arifin <miftahul97@gmail.com>
2021-12-23 15:13:42 +0700

    fix: temp name

---
+++ components/Layout1/index.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import Navbar from '../Navbar';
+import Footer from '../Footer1';
+import ModalLogin from '../ModalLogin';
+import styles from './Layout.module.css';
+
+export default function Layout({ children }) {
+  const [displayChildren, setDisplayChildren] = useState(children);
+  const [transitionStage, setTransitionStage] = useState('fadeOut');
+
+  function handleTransition() {
+    if (transitionStage === 'fadeOut') {
+      setDisplayChildren(children);
+      setTransitionStage('fadeIn');
+    }
+  }
+
+  useEffect(() => {
+    setTransitionStage('fadeIn');
+  }, []);
+
+  useEffect(() => {
+    if (children !== displayChildren) {
+      setTransitionStage('fadeOut');
+    }
+  }, [children, setDisplayChildren, displayChildren]);
+
+  return (
+    <div>
+      <Navbar />
+      <div
+        onTransitionEnd={handleTransition}
+        className={`${styles.content} 
+        ${styles[transitionStage]}`}
+      >
+        {displayChildren}
+      </div>
+      <Footer />
+      <ModalLogin />
+    </div>
+  );
+}

b8229fb91d29b6b921aef381c6d34704af53455a
Miftahul Arifin <miftahul97@gmail.com>
2022-01-15 13:25:56 +0700

    fix: format

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,19 +1,21 @@
 const Button = ({ title, isDisabled, onClick }) => (
-    <button
-        className={`
+  <button
+    className={`
         py-2 px-6 w-full rounded-full text-base text-primary-content font-medium
         bg-gradient-to-r from-primary to-accent
-        ${isDisabled
+        ${
+          isDisabled
             ? `cursor-default`
             : `hover:from-secondary hover:to-accent
                 transition transform hover:scale-105
                 motion-reduce:transition-none motion-reduce:transform-none`
         }
         `}
-        disabled={isDisabled}
-        onClick={onClick}>
-        {title}
-    </button>
-    );
-    
-    export default Button;
+    disabled={isDisabled}
+    onClick={onClick}
+  >
+    {title}
+  </button>
+);
+
+export default Button;

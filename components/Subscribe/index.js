a87acde4a2e24ec2e43515bfe1f1a6bd8659c021
Miftahul Arifin <miftahul97@gmail.com>
2022-01-27 09:08:46 +0700

    feat: update ui front subscribe and card image

--- components/Subscribe/index.js
+++ components/Subscribe/index.js
@@ -1,59 +1,60 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Subscribe() {
-    const [email, setEmail] = useState('');
+  return (
+    <footer className="flex justify-center px-4 py-6 md:py-10 lg:py-12">
+      <div className="container py-8">
+        <h1 className="w-2/3 md:w-3/5 lg:w-1/2 mx-auto text-primary font-normal text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+          Yuk tetap update info kelas dan beasiswa oversea lainnya!
+        </h1>
 
-    var btnOnCLick = (e) => {
-        e.preventDefault();
-        (email == "")
-            ? alert("Masukkan email kamu!")
-            : alert(`${email} berhasil subscribe!`);
-    };
-
-    return (
-        <footer className="flex justify-center px-4 py-6 md:py-10 lg:py-12">
-            <div className="container py-8">
-                <h1 className="w-2/3 md:w-3/5 lg:w-1/2 mx-auto text-primary font-normal text-xl sm:text-2xl md:text-3xl lg:text-4xl">
-                    Yuk tetap update info kelas dan beasiswa oversea lainnya!
-                </h1>
-
-                <div className="flex justify-center mt-6">
-                    <div className="
-                        relative rounded-full
-                        w-2/3 md:w-3/5 lg:w-1/2
-                        md:focus-within:ring
-                        md:focus-within:ring-accent
-                        md:focus-within:ring-opacity-40">
-                        <form className="flex flex-wrap md:flex-row md:border-2 rounded-full border-primary">
-                            <input
-                                type="email"
-                                required
-                                placeholder="subscribe email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                className="
-                                rounded-full
-                                w-full md:w-2/3
-                                mb-2 md:my-1 py-2 px-8 text-gray-700 text-sm
-                                border-2 md:border-none
-                                bg-transparent appearance-none focus:outline-none focus:placeholder-transparent"
-                            />
-                            <button
-                                onClick={btnOnCLick}
-                                className="
-                                static md:absolute top-0 right-0 position
-                                p-2 rounded-full
-                                w-full mx-auto md:w-1/3 h-full
-                                text-sm text-primary-content font-medium italic
-                                bg-gradient-to-r from-primary to-accent
-                                hover:from-secondary hover:to-accent
-                            ">
-                                Subscribe
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+        <div className="flex justify-center mt-6">
+          <div
+            className="
+              relative rounded-full
+              w-2/3 md:w-3/5 lg:w-1/2
+              md:focus-within:ring
+              md:focus-within:ring-accent
+              md:focus-within:ring-opacity-40
+            "
+          >
+            <form
+              action="https://academy.us14.list-manage.com/subscribe/post?u=ee6ef6969f83393d6b9d24455&amp;id=7d0e17a00a"
+              method="post"
+              className="flex flex-wrap md:flex-row md:border-2 rounded-full border-primary"
+              target="_blank"
+              noValidate
+            >
+              <input
+                type="email"
+                required
+                placeholder="subscribe email"
+                name="EMAIL"
+                className="
+                  rounded-full
+                  w-full md:w-2/3
+                  mb-2 md:my-1 py-2 px-8 text-gray-700 text-sm
+                  border-2 md:border-none
+                  bg-transparent appearance-none focus:outline-none focus:placeholder-transparent
+                "
+              />
+              <button
+                type="submit"
+                className="
+                  static md:absolute top-0 right-0 position
+                  p-2 rounded-full
+                  w-full mx-auto md:w-1/3 h-full
+                  text-sm text-primary-content font-medium italic
+                  bg-gradient-to-r from-primary to-accent
+                  hover:from-secondary hover:to-accent
+                "
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- components/TextInput/index.js
+++ components/TextInput/index.js
@@ -1,22 +1,23 @@
 const TextInput = ({ placeholder, value, onChange }) => (
-    <div className="
-        my-3 p-0.5 w-full rounded-full
-        bg-gradient-to-r from-primary to-accent
-        focus-within:ring focus-within:ring-accent focus-within:ring-opacity-40
-    ">
-        <input
-            required
-            placeholder={placeholder}
-            value={value}
-            onChange={onChange}
-            className="
-            w-full rounded-full py-2 px-4
-            bg-primary-content text-gray-700 text-sm
-            focus:outline-none focus:placeholder-transparent
-            "
-        />
-    </div>
-  );
-  
-  export default TextInput;
-  
+  <div
+    className="
+      my-3 p-0.5 w-full rounded-full
+      bg-gradient-to-r from-primary to-accent
+      focus-within:ring focus-within:ring-accent focus-within:ring-opacity-40
+    "
+  >
+    <input
+      required
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="
+        w-full rounded-full py-2 px-4
+        bg-primary-content text-gray-700 text-sm
+        focus:outline-none focus:placeholder-transparent
+      "
+    />
+  </div>
+);
+
+export default TextInput;

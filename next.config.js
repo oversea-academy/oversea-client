285dcea000b297b8959bc1b32f6cefcfe0d13cdf
Miftahul Arifin <miftahul97@gmail.com>
2022-04-10 15:32:16 +0700

    fix: next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ module.exports = {
     SERVER: process.env.SERVER,
     API_URL: process.env.API_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_TAG_MANAGER: process.env.GOOGLE_TAG_MANAGER
+    GOOGLE_TAG_MANAGER_ID: process.env.GOOGLE_TAG_MANAGER_ID,
+    GOOGLE_TAG_MANAGER_AUTH: process.env.GOOGLE_TAG_MANAGER_AUTH,
+    GOOGLE_TAG_MANAGER_PREVIEW: process.env.GOOGLE_TAG_MANAGER_PREVIEW
   },
   async rewrites() {
     return [

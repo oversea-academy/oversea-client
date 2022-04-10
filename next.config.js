6d988c0c10fe8cefe1d62d29ca5add939c4d45a0
Miftahul Arifin <miftahul97@gmail.com>
2022-04-10 14:09:41 +0700

    fix: constants

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     SERVER: process.env.SERVER,
-    IS_LIVE: process.env.SERVER === 'production' ? true : false,
     API_URL: process.env.API_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_TAG_MANAGER: process.env.GOOGLE_TAG_MANAGER
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import TagManager from 'react-gtm-module';
-import Config from '../next.config';
+import { GOOGLE_TAG_MANAGER, IS_LIVE } from '../constants';
 import Layout from '../components/Layout';
 import store from '../store';
 
@@ -10,12 +10,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 
 const tagManagerArgs = {
-  id: Config.env.GOOGLE_TAG_MANAGER
+  id: GOOGLE_TAG_MANAGER
 };
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    if (Config.env.IS_LIVE) {
+    if (IS_LIVE) {
       TagManager.initialize(tagManagerArgs);
     }
   }, []);

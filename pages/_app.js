23d5c275b9e4434bee5f70d1f8f247162a29d0fd
Miftahul Arifin <miftahul97@gmail.com>
2022-04-10 13:46:51 +0700

    feat: add google tag menager

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
+import TagManager from 'react-gtm-module';
+import Config from '../next.config';
 import Layout from '../components/Layout';
 import store from '../store';
 
@@ -6,7 +9,17 @@ import 'tailwindcss/tailwind.css';
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 
+const tagManagerArgs = {
+  id: Config.env.GOOGLE_TAG_MANAGER
+};
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (Config.env.IS_LIVE) {
+      TagManager.initialize(tagManagerArgs);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <Layout>
